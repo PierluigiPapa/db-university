e98fcd5f95ebb34e57984166081a6271337b7000
PierluigiPapa <pierluigipapa9@gmail.com>
2024-03-04 14:59:12 +0100

    feat: add index.php with exercise about query with Group by

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ WHERE (YEAR(CURRENT_DATE())-YEAR(`date_of_birth`)) > 30;
 
 // 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea 
 SELECT * FROM `courses` 
-WHERE `year` = 1 And `period` = 'I semestre';
+WHERE `year` = 1 AND `period` = 'I semestre';
 
 // 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
 SELECT * FROM `exams`
@@ -39,5 +39,34 @@ AS `insegnanti_senza_telefono`
 FROM `teachers`;
 
 // SECTION EXERCISE - QUERY WITH SELECT // 
+
+
+// SECTION EXERCISE - QUERY WITH GROUP BY // 
+
+// 1. Contare quanti iscritti ci sono stati ogni anno
+SELECT YEAR(`enrolment_date`) AS anno_universitario, COUNT(*) AS numero_iscrizioni
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
+
+// 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT `office_address` AS indirizzo_ufficio, COUNT(*) AS numero_insegnanti
+FROM `teachers`
+GROUP BY `office_address`
+HAVING `numero_insegnanti` > 1;
+
+// 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT `exam_id` AS appello_esame, ROUND(AVG(`vote`)) AS media_voto
+FROM `exam_student`
+GROUP BY `appello_esame`;
+
+
+// 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id` AS numero_dipartimento, COUNT(*) AS numero_corsi
+FROM `degrees`
+GROUP BY `numero_dipartimento`;
+
+// SECTION EXERCISE - QUERY WITH GROUP BY // 
+
 ?>
 
